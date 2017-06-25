d2539eb6576faa7a53dcbdfa0fdb9cbff8b9d77f
Cosme Fulanito <augus.recalde097@gmail.com>
2017-06-25 13:23:15 -0300

    Limpieza de barra lateral

--- forms-abm/registar-alumno.php
+++ forms-abm/registar-alumno.php
@@ -1,46 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html>
+<head>
+	<meta charset="utf-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
     <meta name="author" content="GeeksLabs">
-    <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
-    <link rel="shortcut icon" href="img/favicon.png">
-
-    <title>Basic Table | Creative - Bootstrap 3 Responsive Admin Template</title>
-
-    <!-- Bootstrap CSS -->    
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal"> -->
+    <link rel="shortcut icon" href="../img/favicon.png">
+	<title>REGISTRO DE ALUMNO</title>
+	 <!-- Bootstrap CSS -->
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
     <!-- bootstrap theme -->
-    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link href="../css/bootstrap-theme.css" rel="stylesheet">
     <!--external css-->
     <!-- font icon -->
-    <link href="css/elegant-icons-style.css" rel="stylesheet" />
-    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <link href="../css/elegant-icons-style.css" rel="stylesheet" />
+    <link href="../css/font-awesome.min.css" rel="stylesheet" />
+    <!-- date picker -->
+    
+    <!-- color picker -->
+    
     <!-- Custom styles -->
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/style-responsive.css" rel="stylesheet" />
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 -->
-    <!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-      <script src="js/respond.min.js"></script>
-      <script src="js/lte-ie7.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-  <!-- container section start -->
-  <section id="container" class="">
-      <!--header start-->
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/style-responsive.css" rel="stylesheet" />
+</head>
+<body><center>
 <header class="header dark-bg">
             <div class="toggle-nav">
                 <div class="icon-reorder tooltips" data-original-title="Toggle Navigation" data-placement="bottom"><i class="icon_menu"></i></div>
             </div>
 
             <!--logo start-->
-            <a href="index.html" class="logo">Nice <span class="lite">Admin</span></a>
+            <a href="../index.html" class="logo">SAI <span class="lite">nt</span></a>
             <!--logo end-->
 
             <div class="nav search-row" id="top_menu">
@@ -63,12 +54,12 @@
                     <li id="task_notificatoin_bar" class="dropdown">
                         <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                             <i class="icon-task-l"></i>
-                            <span class="badge bg-important">6</span>
+                            <span class="badge bg-important">5</span>
                         </a>
                         <ul class="dropdown-menu extended tasks-bar">
                             <div class="notify-arrow notify-arrow-blue"></div>
                             <li>
-                                <p class="blue">You have 6 pending letter</p>
+                                <p class="blue">You have 5 pending tasks</p>
                             </li>
                             <li>
                                 <a href="#">
@@ -279,7 +270,7 @@
                                 <a href="#"><i class="icon_chat_alt"></i> Chats</a>
                             </li>
                             <li>
-                                <a href="index.php"><i class="icon_key_alt"></i> Log Out</a>
+                                <a href="login.html"><i class="icon_key_alt"></i> Log Out</a>
                             </li>
                             <li>
                                 <a href="documentation.html"><i class="icon_key_alt"></i> Documentation</a>
@@ -297,145 +288,91 @@
       <!--header end-->
 
       <!--sidebar start-->
-      <aside>
+        <aside>
           <div id="sidebar"  class="nav-collapse ">
               <!-- sidebar menu start-->
               <ul class="sidebar-menu">                
                   <li class="active">
-                      <a class="" href="index_admin.php">
+                      <a class="" href="../index_admin.php">
                           <i class="icon_house_alt"></i>
                           <span>Inicio</span>
                       </a>
                   </li>          
                   <li class="sub-menu">
-                        <a class="" href="basic_table.html">
+                        <a class="" href="../basic_table.html">
                           <i class="icon_table"></i>
                           <span>Gestor de Alumnos</span>
                         </a>
-    
                   
               </ul>
               <!-- sidebar menu end-->
           </div>
       </aside>
 
-      <!--main content start-->
+      <!--Comienzo del contenido-->
       <section id="main-content">
           <section class="wrapper">
-          <div class="row">
-                <div class="col-lg-12">
-                    <h3 class="page-header"><i class="fa fa-table"></i> Detalle Alumnos</h3>
-                </div>
-            </div>
-              <!-- page start-->
-              
-              </div>
+      <div class="row">
+<div class="col-lg-12">
+					<h3 class="page-header"><i class="fa fa-file-text-o"></i>REGISTO DE NUEVO ALUMNO</h3>
+				</div>
+			</div>
               <div class="row">
                   <div class="col-lg-12">
                       <section class="panel">
                           <header class="panel-heading">
-                            <a class="btn btn-primary" href="forms-abm/registar-alumno.php" title="Agregar un nuevo alumno">Agregar</a>
-                              Curso 4 IV
+                             DATOS PERSONALES
                           </header>
-                          <table class="table table-striped table-advance table-hover">
-                           <tbody>
-                              <tr>
-                                 <th><i class="icon_profile"></i> Apellido y Nombre</th>
-                                 <th></i> DNI</th>
-                                 <th><i class="icon_mobile"></i> Telefono</th>
-                                 <th><i class="icon_mobile"></i>Telefono Tutor</th>
-                                 <th><i class="icon_mobile"></i> Asistencia </th>
-                                 <th><i class="icon_cogs"></i>Accion</th>
-                              </tr>
-                              <tr>
-                                 <td>Angeline Mcclain</td>
-                                 <td>234234234</td>
-                                 <td>3704897632</td>
-                                 <td>3704897632</td>
-                                 <td>Presente</td>
-                                 <td>
-                                  <div class="btn-group">
-                                      <a class="btn btn-primary" href="forms-abm\registar-alumno.php" title = "Modificar"><i class="icon_plus_alt2"></i></a>
-                                      
-                                      <a class="btn btn-danger" href="" title = "Eliminar"><i class="icon_close_alt2"></i></a>
+                          <div class="panel-form col-lg-12">
+                              <form class="form-horizontal" method="get" align-items = "center">
+                              
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label">Nombre</label>
+                                      <div class="col-sm-10">
+                                          <input type="text" class="form-control round-input">
+                                      </div>
                                   </div>
-                                  </td>
-                              </tr>
-                              <tr>
-                                 <td>Sung Carlson</td>
-                                 <td>234234234</td>
-                                 <td>3704897632</td>
-                                 <td>3704897632</td>
-                                 <td>Presente</td>
-                                 <td>
-                                  <div class="btn-group">
-                                      <a class="btn btn-primary" href="#" title = "Modificar"><i class="icon_plus_alt2"></i></a>
-                                      
-                                      <a class="btn btn-danger" href="#" title="Eliminar"><i class="icon_close_alt2"></i></a>
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label" color = "#E8E8E8">Apellido</label>
+                                      <div class="col-sm-10">
+                                          <input type="text" class="form-control round-input">
+                                      </div>
                                   </div>
-                                  </td>
-                              </tr>
-                              <tr>
-                                 <td>Bryon Osborne</td>
-                                 <td>234234234</td>
-                                 <td>3704897632</td>
-                                 <td>3704897632</td>
-                                 <td>Ausente</td>
-                                 <td>
-                                 <div class="btn-group">
-                                      <a class="btn btn-primary" href="#" title = "Modificar"><i class="icon_plus_alt2"></i></a>
-                                      
-                                      <a class="btn btn-danger" href="#" title="Eliminar"><i class="icon_close_alt2"></i></a>
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label">DNI</label>
+                                      <div class="col-sm-10">
+                                          <input type="text" class="form-control round-input">
+                                      </div>
                                   </div>
-                                  </td>
-                              </tr>
-                              <tr>
-                                 <td>Dalia Marquez</td>
-                                 <td>234234234</td>
-                                 <td>3704897632</td>
-                                 <td>3704897632</td>
-                                 <td>Presente</td>
-                                 <td>
-                                  <div class="btn-group">
-                                      <a class="btn btn-primary" href="#" title = "Modificar"><i class="icon_plus_alt2"></i></a>
-                                      
-                                      <a class="btn btn-danger" href="#" title="Eliminar"><i class="icon_close_alt2"></i></a>
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label">Sexo</label>
+                                      <div class="btn-group">
+                                                  <a class="btn btn-default" href="" title="Bootstrap 3 themes generator">Seleccionar</a>
+                                                  <a class="btn btn-default dropdown-toggle" data-toggle="dropdown" href="" title="Bootstrap 3 themes generator"><span class="caret"></span></a>
+                                                  <ul class="dropdown-menu">
+                                                    <li><a href="" title="Bootstrap 3 themes generator">Femenino</a></li>
+                                                    <li class="divider"></li>
+                                                    <li><a href="" title="Bootstrap 3 themes generator">Masculino</a></li>
+                                                  </ul>
+                                            </div>
                                   </div>
-                                  </td>
-                              </tr>
-                                              
-                           </tbody>
-                        </table>
+                                  <div class="form-group">
+                                      <label class="col-sm-2 control-label">Nacionalidad</label>
+                                      <div class="col-sm-10">
+                                          <input type="text" class="form-control round-input">
+                                      </div>
+                                  </div>
+                              </form>
+                          </div>
                       </section>
-                  </div>
-              </div>
-              <!-- page end-->
-          </section>
-      </section>
-      <!--main content end-->
-      <div class="text-right">
-            <div class="credits">
-                
-            </div>
-        </div>
-  </section>
-      <!--main content end-->
-      <div class="text-right">
-            <div class="credits">
-                
-            </div>
-        </div>
-  </section>
-  <!-- container section end -->
     <!-- javascripts -->
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- nicescroll -->
-    <script src="js/jquery.scrollTo.min.js"></script>
-    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <!-- nice scroll -->
+    <script src="../js/jquery.scrollTo.min.js"></script>
+    <script src="../js/jquery.nicescroll.js" type="text/javascript"></script>
     <!--custome script for all page-->
-    <script src="js/scripts.js"></script>
+    <script src="../js/scripts.js"></script>
 
-
-  </body>
+</center></body>
 </html>
